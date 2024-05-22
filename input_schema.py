2df93bef6aefa2520d42964d0ac9d52d38b0b1e6
Karthikeyan Subbaraj <karthik.moc@gmail.com>
2024-05-22 04:29:44 +0000

    Input Schema Changes

--- input_schema.py
+++ input_schema.py
@@ -1,8 +1,8 @@
 INPUT_SCHEMA = {
-    "audio_url": {
+    "audio": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["http://thepodcastexchange.ca/s/Porsche-Macan-July-5-2018-1.mp3"]
+        'example': ["<Base64String>"]
     }
 }
